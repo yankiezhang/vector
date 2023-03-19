e245030b8d970774b3626322602585629d38943c
yang <yankiezhang@gmail.com>
2023-03-20 00:04:24 +0800

    Update test()

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -9,5 +9,15 @@
 
 void test() {
     vector<int, 5> v;
+    vector<int, 5> v1 = {1};
+    
+    std::cout << v << std::endl
+    << v1 << std::endl;
+    
+    v +=v1;
     std::cout << v << std::endl;
+
+    v *= 5;
+    std::cout << v << std::endl;
+    
 }
